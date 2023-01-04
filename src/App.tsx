c3b497576ade44a8afb7f76281dd3afb03b690b9
mehtab39 <mehtabgill122@gmail.com>
2023-01-04 15:13:27 +0530

    added react router to support mutiple pages in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { BrowserRouter as Router, useRoutes } from 'react-router-dom';
 import Instruments from './components/instruments';
 import Quotes from './components/quotes';
 
+const components = [
+  { path: '/', element: <Instruments /> },
+  { path: '/quotes/:instrument', element: <Quotes /> },
+];
 export default function App() {
   return (
-    <div className="App">
-      <Instruments />
-      <Quotes/>
-    </div>
+    <Router>
+      <AppInternal />
+    </Router>
   );
 }
+
+const AppInternal = () => {
+  let routes = useRoutes(components);
+  return routes;
+};
--- src/components/quotes/index.tsx
+++ src/components/quotes/index.tsx
@@ -1,12 +1,17 @@
 import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { fetchQuote } from '../../utils/fetch';
 import { IQutoes } from '../../utils/types';
 
 const Quotes = () => {
+  let { instrument } = useParams();
   const [quote, setQuote] = useState<IQutoes | null>(null);
+  console.log('quote:', quote)
   const getQuote = async () => {
-    const data = await fetchQuote('BRITANNIA');
-    setQuote(data);
+    if (instrument) {
+      const data = await fetchQuote(instrument);
+      setQuote(data);
+    }
   };
 
   useEffect(() => {
